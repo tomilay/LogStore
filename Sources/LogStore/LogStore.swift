8835e20e04fcf6253f4c18390c6bd4ad0a14e740
tomilay <tomilay@yahoo.com>
2024-04-13 13:31:00 -0500

    functioning version with log trigger and presentation

--- Sources/LogStore/LogStore.swift
+++ Sources/LogStore/LogStore.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+struct LogStore {
+    static var log: [String] = []
+}
+
+public func printLog(_ string: String) {
+    print(string)
+    
+    LogStore.log.append(string)
+}
---
+++ Sources/LogStore/LogViewController.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thomas Nyongesa on 4/12/24.
+//
+
+import UIKit
+
+public class LogViewController: UITableViewController {
+    let logItems = LogStore.log
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return logItems.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = logItems[indexPath.row]
+        return cell
+    }
+}
